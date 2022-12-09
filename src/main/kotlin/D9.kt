9c7532c12036bc7e05ba9f22d2ac707505b960fa
matejuh <matej.plch@productboard.com>
2022-12-09 09:34:57 +0100

    D9b

--- src/main/kotlin/D9.kt
+++ src/main/kotlin/D9.kt
@@ -4,10 +4,9 @@ fun main() {
     d9a()
 }
 
-val head = Knot()
-val tail = Knot()
+val knots = List(10) { Knot() }
 val tailPositions = HashSet<Position>().apply {
-    add(Position(tail.x, tail.y))
+    add(Position(0, 0))
 }
 
 private fun d9a() {
@@ -24,73 +23,79 @@ private fun d9a() {
     println(tailPositions.size)
 }
 
-private fun touching(): Boolean = (head.x in tail.x - 1..tail.x + 1) && (head.y in tail.y - 1..tail.y + 1)
+private fun touching(first: Knot, second: Knot): Boolean = (first.x in second.x - 1..second.x + 1) && (first.y in second.y - 1..second.y + 1)
 
 private fun fixTailPosition() {
-    if (!touching()) {
-        // vertical move
-        if (head.x == tail.x) {
-            if (head.y > tail.y) {
-                tail.y++
-            } else {
-                tail.y--
-            }
-            // horizontal move
-        } else if (head.y == tail.y) {
-            if (head.x > tail.x) {
-                tail.x++
-            } else {
-                tail.x--
-            }
-        } else {
-            if (head.y > tail.y) {
-                if (head.x > tail.x) {
-                    tail.x ++
-                    tail.y ++
+    for (i in 1 until knots.size) {
+        val head = knots[i-1]
+        val tail = knots[i]
+        if (!touching(head, tail)) {
+            // vertical move
+            if (head.x == tail.x) {
+                if (head.y > tail.y) {
+                    tail.y++
                 } else {
-                    tail.x --
-                    tail.y ++
+                    tail.y--
+                }
+                // horizontal move
+            } else if (head.y == tail.y) {
+                if (head.x > tail.x) {
+                    tail.x++
+                } else {
+                    tail.x--
                 }
             } else {
-                if (head.x > tail.x) {
-                    tail.x ++
-                    tail.y --
+                if (head.y > tail.y) {
+                    if (head.x > tail.x) {
+                        tail.x++
+                        tail.y++
+                    } else {
+                        tail.x--
+                        tail.y++
+                    }
                 } else {
-                    tail.x --
-                    tail.y --
+                    if (head.x > tail.x) {
+                        tail.x++
+                        tail.y--
+                    } else {
+                        tail.x--
+                        tail.y--
+                    }
                 }
+            }
+            if (i == knots.size - 1) {
+                tailPositions.add(Position(tail.x, tail.y))
             }
         }
-        tailPositions.add(Position(tail.x, tail.y))
     }
 }
 
 private fun move(x: Int, y: Int) {
     if (x > 0) {
         for (i in 1..x) {
-            head.x++
+            knots.first().x++
             fixTailPosition()
         }
     } else {
         for (i in -1 downTo x) {
-            head.x--
+            knots.first().x--
             fixTailPosition()
         }
     }
 
     if (y > 0) {
         for (i in 1..y) {
-            head.y++
+            knots.first().y++
             fixTailPosition()
         }
     } else {
         for (i in -1 downTo y) {
-            head.y--
+            knots.first().y--
             fixTailPosition()
         }
     }
-
-    println("head: $head tail: $tail")
+    knots.forEachIndexed { i, knot -> print("$i: $knot" ) }
+    println()
 }
 
 data class Knot(var x: Int = 0, var y: Int = 0)
