b4ed33c3fba9e3667bbf24f58bdddbbb18f1a8a3
matejuh <matej.plch@productboard.com>
2022-12-10 10:48:06 +0100

    d10b

--- src/main/kotlin/D10.kt
+++ src/main/kotlin/D10.kt
@@ -15,7 +15,6 @@ private fun d10a() {
         }
         cpu.process(cmd)
     }
-    println(cpu.cyclesSum)
 }
 
 private interface Cmd
@@ -25,28 +24,47 @@ private object Noop: Cmd
 data class Add(val value: Int): Cmd
 
 private class Processor {
-    private var cycles = setOf(20, 60, 100, 140, 180, 220)
-    var cyclesSum = 0
-
     private var cycle = 0
-    private var sum = 1
+
+    private var spriteBeginning = 1
 
     fun process(cmd: Cmd) {
         when(cmd) {
-            is Noop -> incrementCycle()
+            is Noop -> {
+                incrementCycle()
+            }
             is Add -> {
                 incrementCycle()
                 incrementCycle()
-                sum += cmd.value
+                val tmp = spriteBeginning + cmd.value
+                spriteBeginning = tmp
             }
         }
     }
 
     private fun incrementCycle() {
-        cycle ++
-        println("cycle: $cycle $sum")
-        if (cycles.contains(cycle)) {
-            cyclesSum += (cycle * sum)
+        if ((cycle % 40) in (spriteBeginning -1) .. (spriteBeginning + 1)) {
+            print("#")
+        } else {
+            print(" ")
         }
+        if (cycle % 40 == 39) {
+            println()
+        }
+        cycle ++
     }
 }
+
+//##..##..##..##..##..##..##..##..##..##..
+//###...###...###...###...###...###...###.
+//####....####....####....####....####....
+//#####.....#####.....#####.....#####.....
+//######......######......######......####
+//#######.......#######.......#######.....
+
+//###  ####  ##  ###  #  # ###  #### ###
+//#  #    # #  # #  # # #  #  # #    #  #
+//#  #   #  #    #  # ##   #  # ###  ###
+//###   #   # ## ###  # #  ###  #    #  #
+//#    #    #  # #    # #  #    #    #  #
+//#    ####  ### #    #  # #    #### ###
