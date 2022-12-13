fdd9c065ebd22e07494433558712d68f7d6d3f08
matejuh <matej.plch@productboard.com>
2022-12-13 23:47:57 +0100

    d12a

--- src/main/kotlin/D12.kt
+++ src/main/kotlin/D12.kt
@@ -7,7 +7,7 @@ fun main() {
 private var shortestPath: Int = -1
 
 fun d12a() {
-    val columns = File(ClassLoader.getSystemResource("D12test").path).readLines()
+    val columns = File(ClassLoader.getSystemResource("D12").path).readLines()
         .mapIndexed { idy, row ->
             row.toCharArray().mapIndexed { idx, ch -> Node(ch, Pair(idy, idx)) }
         }
@@ -17,11 +17,11 @@ fun d12a() {
             if (node.sign == 'S') {
                 start = node
             }
-            val children = listOf(idy + 1 to idx, idy - 1 to idx, idy to idx - 1, idy to idx + 1)
+            val children = listOf(idy + 1 to idx, idy to idx - 1, idy - 1 to idx, idy to idx + 1)
                 .mapNotNull { (y, x) ->
                     if (y >= 0 && y < columns.size && x >= 0 && x < row.size) {
                         val child = columns[y][x]
-                        if (child.value - node.value in 0..1) {
+                        if (child.value - node.value <= 1) {
                             child
                         } else {
                             null
@@ -31,41 +31,51 @@ fun d12a() {
                     }
                 }
             if (children.isNotEmpty()) {
-                node.children.addAll(children.sortedByDescending { it.value })
+                node.children.addAll(children)
             }
         }
     }
 
 //    columns.forEach { row -> row.forEach { node -> println("${node.sign} ${node.value} ${node.children}")} }
 
-    iterate(start, emptyList())
+    iterate(start)
+//    columns.forEach { row ->
+//        row.forEach { node -> print(if (node.path > 0) "1" else node.sign ) }
+//        println()
+//    }
+
     println(shortestPath)
 }
 
-private fun iterate(node: Node, visitedNodes: List<Node>) {
-//    println("$node ${visitedNodes.size}")
-    if (node.sign == 'E') {
-        println("Found " + visitedNodes.size)
-        if (shortestPath == -1 || visitedNodes.size < shortestPath) {
-            shortestPath = visitedNodes.size
-        }
-    } else {
-        if (shortestPath != -1 && visitedNodes.size > shortestPath) {
+private fun iterate(node: Node) {
+    val queue = ArrayDeque<Node>()
+    queue.add(node)
+    val visited = mutableSetOf<Node>()
+
+
+    while (queue.isNotEmpty()) {
+        val currentNode = queue.removeFirst()
+
+        if (visited.contains(currentNode)) continue
+        if (currentNode.sign == 'E') {
+            shortestPath = currentNode.path
             return
         } else {
-            val nodes = visitedNodes + node
-            node.children.filter { !visitedNodes.contains(it) }.forEach {
-                iterate(it, nodes)
+            currentNode.children.forEach {
+                it.path = currentNode.path + 1
+                queue.addLast(it)
             }
         }
+        visited.add(currentNode)
     }
 }
 
 private data class Node(val sign: Char, val coords: Pair<Int, Int>) {
     val value: Int = when (sign) {
-        'S' -> 0
-        'E' -> 27
-        else -> sign.code - 96
-    }
+        'S' -> 'a'.code
+        'E' -> 'z'.code
+        else -> sign.code
+    } - 'a'.code + 1
     val children = mutableListOf<Node>()
+    var path = 0
 }
