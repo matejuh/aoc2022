1f7e51abba1e250ea02f91779ab06005594b1f45
matejuh <matej.plch@productboard.com>
2022-12-15 19:19:14 +0100

    d15

--- src/main/kotlin/D15.kt
+++ src/main/kotlin/D15.kt
@@ -1,123 +1,73 @@
 import java.io.File
 import kotlin.math.abs
+import kotlin.math.max
 
 fun main() {
-    d15a()
-}
-
-private interface Positioned {
-    val x: Int
-    val y: Int
-    val sign: String
-}
-private data class Sensor(override val x: Int, override val y: Int): Positioned {
-    override val sign: String
-        get() = "S"
-}
-private data class Beacon(override val x: Int, override val y: Int): Positioned {
-    override val sign: String
-        get() = "B"
-}
-
-private data class Signal(override val x: Int, override val y: Int): Positioned {
-    override val sign: String
-        get() = "#"
-}
-
-private class Tunnels(firstPoint: Positioned) {
-    private val underground: MutableMap<Pair<Int, Int>, Positioned> = mutableMapOf()
-    var minX: Int = firstPoint.x
-    var maxX: Int = firstPoint.x
-    var minY: Int = firstPoint.y
-    var maxY: Int = firstPoint.y
-
-    fun add(value: Positioned) {
-        val x = value.x
-        val y = value.y
-        underground.putIfAbsent(Pair(x, y), value)
-        if (x < minX) {
-            minX = x
-        }
-        if (x > maxX) {
-            maxX = x
-        }
-        if (y < minY) {
-            minY = y
-        }
-        if (y > maxY) {
-            maxY = y
-        }
-    }
-
-    fun print() {
-        for (y in minY..maxY) {
-            for (x in minX..maxX) {
-                print(underground[Pair(x, y)]?.sign ?: ".")
-            }
-            println()
-        }
-    }
-
-    fun withoutBeacon(yPosition: Int): Int {
-        var counter = 0
-        for (x in minX .. maxX) {
-            if (underground[Pair(x, yPosition)] is Signal) {
-                counter ++
-            }
-        }
-        return counter
-    }
+//    d15a()
+    d15b()
 }
 
 private fun d15a() {
-    lateinit var tunnels: Tunnels
     val regex = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".toRegex()
-    File(ClassLoader.getSystemResource("D15test").path).readLines().mapIndexed { idx, line ->
+    val goal = 2000000
+    val safePoints = mutableSetOf<Int>()
+    File(ClassLoader.getSystemResource("D15").path).readLines().forEach { line ->
         val (sensorX, sensorY, beaconX, beaconY) = regex.matchEntire(line)!!.destructured
-        val sensor = Sensor(sensorX.toInt(), sensorY.toInt())
-        val beacon = Beacon(beaconX.toInt(), beaconY.toInt())
-        if (idx == 0) {
-            tunnels = Tunnels(sensor)
+        val sensor = Position(sensorX.toInt(), sensorY.toInt())
+        val beacon = Position(beaconX.toInt(), beaconY.toInt())
+        val diffX = abs(sensor.x - beacon.x)
+        val diffY = abs( sensor.y - beacon.y)
+        val manhattanDistance = diffX + diffY
+        val yDistanceToGoal = abs(sensor.y - goal)
+        val xDistanceToGoal = manhattanDistance - yDistanceToGoal
+
+        for (covered in sensor.x - xDistanceToGoal .. sensor.x + xDistanceToGoal) {
+            if (beacon != Position(covered, goal)) {
+                safePoints.add(covered)
+            }
         }
-        tunnels.add(sensor)
-        tunnels.add(beacon)
-        calculateCoverage(tunnels, sensor, beacon)
     }
-//    tunnels.print()
-    println(tunnels.withoutBeacon(10))
+    println(safePoints.size)
 }
 
-private fun calculateCoverage(tunnels: Tunnels, sensor: Sensor, beacon: Beacon) {
-    val diffX = abs(sensor.x - beacon.x)
-    val diffY = abs( sensor.y - beacon.y)
-    val distance = diffX + diffY
+private fun d15b() {
+    val min = 0
+    val max = 4000000
+    val regex = "Sensor at x=(-?\\d+), y=(-?\\d+): closest beacon is at x=(-?\\d+), y=(-?\\d+)".toRegex()
+    val safeRangesPerLine = Array<MutableList<IntRange>>(max + 1) { mutableListOf() }
+    File(ClassLoader.getSystemResource("D15").path).readLines().forEach { line ->
+        val (sensorX, sensorY, beaconX, beaconY) = regex.matchEntire(line)!!.destructured
+        val sensor = Position(sensorX.toInt(), sensorY.toInt())
+        val beacon = Position(beaconX.toInt(), beaconY.toInt())
+        val diffX = abs(sensor.x - beacon.x)
+        val diffY = abs( sensor.y - beacon.y)
+        val manhattanDistance = diffX + diffY
 
-    var it = 0
-    for (y in sensor.y .. sensor.y + distance) {
-        for (x in sensor.x..sensor.x + distance - it) {
-            tunnels.add(Signal(x, y))
+        for (y in min .. max) {
+            val distanceToY = abs(sensor.y - y)
+            val xWidth = manhattanDistance - distanceToY
+            if (xWidth > 0) {
+                safeRangesPerLine[y].add(sensor.x - xWidth .. sensor.x + xWidth)
+            }
         }
-        it ++
     }
-    it = 0
-    for (y in sensor.y .. sensor.y + distance) {
-        for (x in sensor.x downTo sensor.x - distance + it) {
-            tunnels.add(Signal(x, y))
+
+    val signal = safeRangesPerLine.mapIndexedNotNull { y, safeRanges ->
+        val sorted = safeRanges.sortedBy { it.first }
+        var previousRangeEnd = sorted.first().last
+        val rangeWithGap = sorted.drop(1).find { range ->
+            if (range.first > previousRangeEnd) {
+                true
+            } else {
+                previousRangeEnd = max(previousRangeEnd, range.last)
+                false
+            }
         }
-        it ++
-    }
-    it = 0
-    for (y in sensor.y downTo  sensor.y - distance) {
-        for (x in sensor.x downTo sensor.x - distance + it) {
-            tunnels.add(Signal(x, y))
+        if (rangeWithGap == null) {
+            null
+        } else {
+            (rangeWithGap.first - 1) * 4000000L + y
         }
-        it ++
-    }
-    it = 0
-    for (y in sensor.y downTo  sensor.y - distance) {
-        for (x in sensor.x .. sensor.x + distance - it) {
-            tunnels.add(Signal(x, y))
-        }
-        it ++
-    }
+    }.first()
+    println(signal)
 }
